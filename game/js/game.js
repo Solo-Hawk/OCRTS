3a32f36faa5943aaca14ddfca15caa1384b58745
Solo-Hawk <Solo-Hawk@users.noreply.github.com>
2018-11-01 13:24:15 +0000

    Setting up Basic JS

---
+++ game/js/game.js
@@ -0,0 +1,5 @@
+// JavaScript source code
+var canvas = document.getElementById('stage');
+var ctx = canvas.getContext('2d');
+ctx.canvas.width  = window.innerWidth - 100;
+ctx.canvas.height = window.innerHeight - 100;
